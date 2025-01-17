42e8a8d9f73cd612a315e453719c23e50ea8cd41
René van Asperen <rvanasperen@gmail.com>
2025-01-17 13:34:52 +0100

    Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,20 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    /** @use HasFactory<UserFactory> */
+    use HasFactory;
+    use Notifiable;
+
+    #region Eloquent Properties
 
     /**
      * The attributes that are mass assignable.
@@ -34,10 +38,16 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    #endregion
+
+    #region Eloquent Relationships
+
     public function accounts(): HasMany
     {
         return $this->hasMany(Account::class);
     }
+
+    #endregion
 
     /**
      * Get the attributes that should be cast.
