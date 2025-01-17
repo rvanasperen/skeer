893b6642374c693354a38bb89ae67a963a7713e6
René van Asperen <rvanasperen@gmail.com>
2025-01-17 13:39:02 +0100

    Refactor verify email page

--- resources/js/Pages/Auth/VerifyEmail.tsx
+++ resources/js/Pages/Auth/VerifyEmail.tsx
@@ -1,4 +1,4 @@
-import PrimaryButton from '@/Components/PrimaryButton';
+import { Button } from '@/Components/UI/Form';
 import GuestLayout from '@/Layouts/GuestLayout';
 import { Head, Link, useForm } from '@inertiajs/react';
 import { FormEventHandler } from 'react';
@@ -6,7 +6,7 @@ import { FormEventHandler } from 'react';
 export default function VerifyEmail({ status }: { status?: string }) {
     const { post, processing } = useForm({});
 
-    const submit: FormEventHandler = (e) => {
+    const handleSubmit: FormEventHandler = (e) => {
         e.preventDefault();
 
         post(route('verification.send'));
@@ -16,32 +16,30 @@ export default function VerifyEmail({ status }: { status?: string }) {
         <GuestLayout>
             <Head title="Email Verification" />
 
-            <div className="mb-4 text-sm text-gray-600 dark:text-gray-400">
-                Thanks for signing up! Before getting started, could you verify
-                your email address by clicking on the link we just emailed to
-                you? If you didn't receive the email, we will gladly send you
-                another.
-            </div>
-
-            {status === 'verification-link-sent' && (
-                <div className="mb-4 text-sm font-medium text-green-600 dark:text-green-400">
-                    A new verification link has been sent to the email address
-                    you provided during registration.
+            <form
+                className="mx-auto max-w-md space-y-4"
+                onSubmit={handleSubmit}
+            >
+                <div className="text-sm">
+                    Thanks for signing up! Before getting started, could you
+                    verify your email address by clicking on the link we just
+                    emailed to you? If you didn't receive the email, we will
+                    gladly send you another.
                 </div>
-            )}
 
-            <form onSubmit={submit}>
-                <div className="mt-4 flex items-center justify-between">
-                    <PrimaryButton disabled={processing}>
+                {status === 'verification-link-sent' && (
+                    <div className="text-sm text-green-400">
+                        A new verification link has been sent to the email
+                        address you provided during registration.
+                    </div>
+                )}
+
+                <div className="flex items-center justify-between">
+                    <Button disabled={processing}>
                         Resend Verification Email
-                    </PrimaryButton>
+                    </Button>
 
-                    <Link
-                        href={route('logout')}
-                        method="post"
-                        as="button"
-                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
-                    >
+                    <Link href={route('logout')} method="post" as="button">
                         Log Out
                     </Link>
                 </div>
