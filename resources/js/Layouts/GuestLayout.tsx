1e6b254d1517a8dc504f0910932fd5d01e90f31d
René van Asperen <rvanasperen@gmail.com>
2025-01-17 13:40:40 +0100

    Refactor GuestLayout

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,29 +1,36 @@
+import User from '@/Models/User';
 import { Link, usePage } from '@inertiajs/react';
 import { PropsWithChildren } from 'react';
+
+function TopNav({ user }: { user?: User }) {
+    return (
+        <div className="dark:bg-gray-950">
+            <div className="mx-auto flex max-w-7xl items-center justify-between p-4">
+                <Link href={route('index')} className="text-lg font-bold">
+                    Skeer
+                </Link>
+
+                <div className="flex gap-4">
+                    {user ? (
+                        <Link href={route('dashboard')}>Dashboard</Link>
+                    ) : (
+                        <>
+                            <Link href={route('login')}>Login</Link>
+                            <Link href={route('register')}>Register</Link>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default function GuestLayout({ children }: PropsWithChildren) {
     const user = usePage().props.auth.user;
 
     return (
         <div className="min-h-screen bg-gray-800 dark:text-gray-100">
-            <div className="dark:bg-gray-950">
-                <div className="mx-auto flex max-w-7xl items-center justify-between p-4">
-                    <Link href={route('index')} className="text-lg font-bold">
-                        Skeer
-                    </Link>
-
-                    <div className="flex gap-4">
-                        {user ? (
-                            <Link href={route('dashboard')}>Dashboard</Link>
-                        ) : (
-                            <>
-                                <Link href={route('login')}>Login</Link>
-                                <Link href={route('register')}>Register</Link>
-                            </>
-                        )}
-                    </div>
-                </div>
-            </div>
+            <TopNav user={user} />
 
             <div className="mx-auto max-w-7xl p-4">{children}</div>
         </div>
