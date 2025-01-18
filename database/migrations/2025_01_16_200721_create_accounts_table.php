a6360dd056840613cf5dc8a496c09134f02c3f83
René van Asperen <rvanasperen@gmail.com>
2025-01-18 16:46:47 +0100

    Make account number unique

--- database/migrations/2025_01_16_200721_create_accounts_table.php
+++ database/migrations/2025_01_16_200721_create_accounts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('currency_id')->constrained();
             $table->foreignId('user_id')->constrained();
             $table->string('name');
-            $table->string('number');
+            $table->string('number')->unique();
             $table->string('type');
             $table->timestamps();
         });
