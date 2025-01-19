3de32e080c155c7054183cf8644a2e39595a4beb
René van Asperen <rvanasperen@gmail.com>
2025-01-19 22:29:00 +0100

    Working on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\TransactionType;
 use App\Models\Account;
 use App\Models\Transaction;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -12,15 +16,58 @@ class DashboardController
 {
     public function __invoke(Request $request): Response
     {
-        $accounts = $request->user()
-            ->accounts()
+        $startDate = Carbon::parse($request->input('start_date', now()->subMonths(12)));
+        $endDate = Carbon::parse($request->input('end_date', now()));
+        $groupBy = $request->input('group_by', 'month'); // day, week, month, year
+
+        $accounts = $this->getAccounts($request->user());
+        $transactionData = $this->getTransactionData($request->user(), $startDate, $endDate, $groupBy);
+
+        return Inertia::render('Dashboard', [
+            'accounts' => $accounts,
+            'transactionData' => $transactionData,
+            'startDate' => $startDate->toDateString(),
+            'endDate' => $endDate->toDateString(),
+            'groupBy' => $groupBy,
+        ]);
+    }
+
+    private function getAccounts(User $user): Collection
+    {
+        return $user->accounts()
             ->with(['currency',  'transactions'])
             ->orderBy('name')
             ->get();
+    }
 
-        $transactionData = $request->user()
-            ->transactions()
-            ->selectRaw('DATE(transaction_date) AS date, SUM(CASE WHEN type = "Income" THEN amount ELSE -amount END) AS amount')
+    private function getTransactionData(User $user, Carbon $startDate, Carbon $endDate, string $groupBy): array
+    {
+        $query = $user->transactions();
+
+        switch ($groupBy) {
+            case 'day':
+                $query->selectRaw('DATE(transaction_date) AS date');
+                break;
+            case 'week':
+                $query->selectRaw('strftime(\'%Y-%W\', transaction_date) AS date');
+                break;
+            case 'month':
+                $query->selectRaw('strftime(\'%Y-%m\', transaction_date) AS date');
+                break;
+            case 'year':
+                $query->selectRaw('strftime(\'%Y\', transaction_date) AS date');
+                break;
+        }
+
+        return $query
+            ->selectRaw(sprintf(
+                'SUM(CASE WHEN type = "%s" THEN amount ELSE -amount END) AS amount',
+                TransactionType::Income->value
+            ))
+            ->whereBetween('transaction_date', [
+                $startDate,
+                $endDate->addDay(),
+            ])
             ->groupBy('date')
             ->orderBy('date')
             ->get()
@@ -29,10 +76,5 @@ class DashboardController
                 'amount' => round($transaction->amount, 2),
             ])
             ->toArray();
-
-        return Inertia::render('Dashboard', [
-            'accounts' => $accounts,
-            'transactionData' => $transactionData,
-        ]);
     }
 }
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,23 +1,79 @@
 import NetWorthCard from '@/Components/Dashboard/NetWorthCard';
 import NetWorthChartCard from '@/Components/Dashboard/NetWorthChartCard';
 import TransactionsChartCard from '@/Components/Dashboard/TransactionsChartCard';
+import { Button, Input, Select } from "@/Components/UI/Form";
 import AuthenticatedLayout from '@/Layouts/AppLayout';
 import { Account } from '@/Models';
-import { Head } from '@inertiajs/react';
+import { Head, router } from "@inertiajs/react";
+import { useState } from 'react';
 
 export default function Dashboard({
     accounts,
     transactionData,
+    startDate,
+    endDate,
+    groupBy,
 }: {
     accounts: Account[];
     transactionData: { date: string; amount: number }[];
+    startDate: string;
+    endDate: string;
+    groupBy: 'day' | 'week' | 'month' | 'year';
 }) {
+    const [selectedStartDate, setSelectedStartDate] =
+        useState<string>(startDate);
+    const [selectedEndDate, setSelectedEndDate] = useState<string>(endDate);
+    const [selectedGroupBy, setSelectedGroupBy] = useState<
+        'day' | 'week' | 'month' | 'year'
+    >(groupBy);
+
+    const handleFilter = () => {
+        router.get(route('dashboard'), {
+            start_date: selectedStartDate,
+            end_date: selectedEndDate,
+            group_by: selectedGroupBy,
+        });
+    };
+
     return (
         <AuthenticatedLayout>
             <Head title="Dashboard" />
 
             <div className="space-y-8">
                 <div className="text-xl font-bold">Dashboard</div>
+
+                <div className="flex items-center gap-2">
+                    <Input
+                        onChange={(e) => setSelectedStartDate(e.target.value)}
+                        type="date"
+                        value={selectedStartDate}
+                    />
+                    <div>to</div>
+                    <Input
+                        onChange={(e) => setSelectedEndDate(e.target.value)}
+                        type="date"
+                        value={selectedEndDate}
+                    />
+                    <div>grouped by</div>
+                    <Select
+                        onChange={(e) =>
+                            setSelectedGroupBy(
+                                e.target.value as
+                                    | 'day'
+                                    | 'week'
+                                    | 'month'
+                                    | 'year',
+                            )
+                        }
+                        value={selectedGroupBy}
+                    >
+                        <option value="day">Day</option>
+                        <option value="week">Week</option>
+                        <option value="month">Month</option>
+                        <option value="year">Year</option>
+                    </Select>
+                    <Button onClick={handleFilter}>Filter</Button>
+                </div>
 
                 <div className="grid grid-cols-4 gap-8">
                     <div className="col-span-3 flex flex-col gap-8">
