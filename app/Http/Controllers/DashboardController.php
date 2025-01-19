48de3e404e100810c04bbd15f99261f8e84e95ba
René van Asperen <rvanasperen@gmail.com>
2025-01-19 21:30:03 +0100

    Add transactions graph to dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -16,8 +18,21 @@ class DashboardController
             ->orderBy('name')
             ->get();
 
+        $transactionData = $request->user()
+            ->transactions()
+            ->selectRaw('DATE(transaction_date) AS date, SUM(CASE WHEN type = "Income" THEN amount ELSE -amount END) AS total_amount')
+            ->groupBy('date')
+            ->orderBy('date')
+            ->get()
+            ->map(fn ($transaction) => [
+                'date' => $transaction->date,
+                'total_amount' => round($transaction->total_amount, 2),
+            ])
+            ->toArray();
+
         return Inertia::render('Dashboard', [
             'accounts' => $accounts,
+            'transactionData' => $transactionData,
         ]);
     }
 }
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -2,8 +2,29 @@ import { Card } from '@/Components/UI';
 import AuthenticatedLayout from '@/Layouts/AppLayout';
 import { Account, Currency } from '@/Models';
 import { Head } from '@inertiajs/react';
+import {
+    Bar,
+    BarChart,
+    CartesianGrid,
+    ReferenceLine,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from 'recharts';
 
-export default function Dashboard({ accounts }: { accounts: Account[] }) {
+interface TransactionData {
+    date: string;
+    total_amount: number;
+}
+
+export default function Dashboard({
+    accounts,
+    transactionData,
+}: {
+    accounts: Account[];
+    transactionData: TransactionData[];
+}) {
     const enabledCurrencies = Array.from(
         new Set(accounts.map((account) => account.currency?.code)),
     );
@@ -33,6 +54,11 @@ export default function Dashboard({ accounts }: { accounts: Account[] }) {
         });
     }
 
+    const processedTransactionData = transactionData.map((transaction) => ({
+        ...transaction,
+        fill: transaction.total_amount >= 0 ? '#82ca8d' : '#ff6b6b',
+    }));
+
     return (
         <AuthenticatedLayout>
             <Head title="Dashboard" />
@@ -41,58 +67,110 @@ export default function Dashboard({ accounts }: { accounts: Account[] }) {
                 <div className="text-xl font-bold">Dashboard</div>
 
                 <div className="grid grid-cols-4 gap-8">
-                    <Card className="col-span-3">graph</Card>
+                    <div className="col-span-3 flex flex-col gap-8">
+                        <Card className="col-span-3">
+                            <div className="text-xl">Net Worth</div>
+                        </Card>
 
-                    <Card className="space-y-4">
-                        <div>
-                            <div className="text-2xl">Net worth</div>
+                        <Card className="col-span-3">
+                            <div className="text-xl">Transactions</div>
+
+                            <ResponsiveContainer
+                                className="mt-4"
+                                width="100%"
+                                height={400}
+                            >
+                                <BarChart data={processedTransactionData}>
+                                    <CartesianGrid
+                                        stroke="#4a5568"
+                                        strokeDasharray="3 3"
+                                    />
+                                    <XAxis
+                                        dataKey="date"
+                                        stroke="#e2e8f0"
+                                        tick={{ fill: '#e2e8f0' }}
+                                    />
+                                    <YAxis
+                                        stroke="#e2e8f0"
+                                        tick={{ fill: '#e2e8f0' }}
+                                    />
+                                    <Tooltip
+                                        contentStyle={{
+                                            backgroundColor: '#2d3748',
+                                            borderColor: '#4a5568',
+                                            color: '#e2e8f0',
+                                        }}
+                                        cursor={{ fill: '#4a5568' }}
+                                        formatter={(value: number) => [
+                                            new Intl.NumberFormat('en-US', {
+                                                style: 'currency',
+                                                currency: 'EUR',
+                                            }).format(value),
+                                            'Total Amount',
+                                        ]}
+                                        itemStyle={{ color: '#e2e8f0' }}
+                                    />
+                                    <ReferenceLine y={0} stroke="#fc0" />
+                                    <Bar dataKey="total_amount" barSize={30} />
+                                </BarChart>
+                            </ResponsiveContainer>
+                        </Card>
+                    </div>
+
+                    <div>
+                        <Card className="space-y-4">
                             <div>
-                                {Object.keys(networthByCurrency).map(
-                                    (currencyCode, index) => (
-                                        <div key={index}>
-                                            {currencyCode}:{' '}
-                                            {networthByCurrency[currencyCode] >
-                                                0 && (
-                                                <span className="text-green-500">
-                                                    {formatterByCurrency[
-                                                        currencyCode
-                                                    ].format(
-                                                        networthByCurrency[
+                                <div className="text-2xl">Net worth</div>
+                                <div>
+                                    {Object.keys(networthByCurrency).map(
+                                        (currencyCode, index) => (
+                                            <div key={index}>
+                                                {currencyCode}:{' '}
+                                                {networthByCurrency[
+                                                    currencyCode
+                                                ] > 0 && (
+                                                    <span className="text-green-500">
+                                                        {formatterByCurrency[
                                                             currencyCode
-                                                        ],
-                                                    )}
-                                                </span>
-                                            )}
-                                            {networthByCurrency[currencyCode] <=
-                                                0 && (
-                                                <span className="red">
-                                                    {formatterByCurrency[
-                                                        currencyCode
-                                                    ].format(
-                                                        networthByCurrency[
+                                                        ].format(
+                                                            networthByCurrency[
+                                                                currencyCode
+                                                            ],
+                                                        )}
+                                                    </span>
+                                                )}
+                                                {networthByCurrency[
+                                                    currencyCode
+                                                ] <= 0 && (
+                                                    <span className="red">
+                                                        {formatterByCurrency[
                                                             currencyCode
-                                                        ],
-                                                    )}
-                                                </span>
-                                            )}
-                                        </div>
-                                    ),
-                                )}
-                            </div>
-                        </div>
-
-                        <div>
-                            <div className="text-xl">Accounts</div>
-                            {accounts.map((account) => (
-                                <div key={account.id}>
-                                    {account.name}:{' '}
-                                    {formatterByCurrency[
-                                        (account.currency as Currency).code
-                                    ].format(account.balance)}
+                                                        ].format(
+                                                            networthByCurrency[
+                                                                currencyCode
+                                                            ],
+                                                        )}
+                                                    </span>
+                                                )}
+                                            </div>
+                                        ),
+                                    )}
                                 </div>
-                            ))}
-                        </div>
-                    </Card>
+                            </div>
+
+                            <div>
+                                <div className="text-xl">Accounts</div>
+                                {accounts.map((account) => (
+                                    <div key={account.id}>
+                                        {account.name}:{' '}
+                                        {formatterByCurrency[
+                                            (account.currency as Currency).code
+                                        ].format(account.balance)}
+                                    </div>
+                                ))}
+                            </div>
+                        </Card>
+                    </div>
                 </div>
             </div>
         </AuthenticatedLayout>
