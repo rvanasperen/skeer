e3a6bb4fd20bebc27dda9c770ced9a7bfcfa5f9b
René van Asperen <rvanasperen@gmail.com>
2025-01-20 22:23:40 +0100

    Fix group by dropdown text

--- resources/js/Components/UI/DateRangeGroupFilter.tsx
+++ resources/js/Components/UI/DateRangeGroupFilter.tsx
@@ -43,7 +43,7 @@ export default function DateRangeGroupFilter({
                     (key) => {
                         return (
                             <option key={key} value={GroupBy[key]}>
-                                {GroupBy[key]}
+                                {key}
                             </option>
                         );
                     },
