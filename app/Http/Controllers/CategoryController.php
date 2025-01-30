a195bcc60d215e6147015c30d6b6e8b2320ff4e4
Sander van Kasteel <info@sandervankasteel.nl>
2025-01-31 00:31:39 +0100

    Initial implementation

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Domain\Category\CategoryDataBuilder;
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -16,10 +19,59 @@ class CategoryController
 
     public function index(Request $request): Response
     {
-        $categories = $this->builder->buildCategoryData($request->user()); // todo: date range
+        $categories = $this->builder->buildCategoryData($request->user());
 
         return Inertia::render('Category/Index', [
             'categories' => $categories,
         ]);
+    }
+
+    public function create(): Response
+    {
+        /** @var User $user */
+        $user = auth()->user();
+        $categories = $this->builder->buildCategoryData($user);
+
+        return Inertia::render('Category/Create', [
+            'categories' => $categories,
+        ]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'parent_id' => ['nullable', 'exists:categories,id'],
+        ]);
+
+        Category::create($validated + [
+            'user_id' => auth()->id(),
+        ]);
+
+        return to_route('categories.index');
+    }
+
+    public function edit(Category $category): Response
+    {
+        /** @var User $user */
+        $user = auth()->user();
+        $categories = $this->builder->buildCategoryData($user);
+
+        return Inertia::render('Category/Edit', [
+            'categories' => $categories,
+            'category' => $category,
+        ]);
+    }
+
+    public function update(Request $request, Category $category): RedirectResponse
+    {
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'parent_id' => ['nullable', 'exists:categories,id'],
+        ]);
+
+        $category->update($validated);
+
+        return to_route('categories.index');
     }
 }
